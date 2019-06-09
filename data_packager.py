9c60428163e24988d36a1ef138c850ab9ed1fe06
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2019-06-09 18:57:48 -0400

    Add files via upload

---
+++ data_packager.py
@@ -0,0 +1,199 @@
+import csv
+
+#Processing raw precipitation data
+with open('PRCP.csv', 'r') as PRCPInput:
+  with open('PRCP-processed1.csv', 'w') as PRCPOutput:
+    #Iterable 2D array of csv file
+    PRCPReader = csv.reader(
+        PRCPInput, delimiter = ',');
+    PRCPWriter = csv.writer(
+      PRCPOutput, delimiter = ",", quotechar = '"', quoting = csv.QUOTE_ALL);
+
+    stations = []; #Holds station IDs
+    latitudes = []; #Checking for new station locations
+    stationTag = 0; #Stations from 1 - 16
+    outputPRCP = []; #Holds rows for output file
+
+    headings = next(PRCPReader, None); #header row
+    outputPRCP.append(headings);
+
+    for row in PRCPReader: #Checking all PRCP records
+      #If new location found
+      if row[1] not in latitudes:
+        #Update locations and ID tag
+        latitudes.append(row[1]);
+        stationTag += 1;
+        #Add new station to list
+        stations.append((stationTag, row[1], row[2]));
+      
+      row[0] = stationTag;
+      outputPRCP.append(row);
+
+    PRCPWriter.writerows(outputPRCP);
+    PRCPOutput.close();
+  PRCPInput.close(); 
+
+#Processing raw temperature data
+with open('Temp.csv', 'r') as tempInput:
+  with open('Temp-processed1.csv', 'w') as tempOutput:
+    #Iterable 2D array of csv file
+    tempReader = csv.reader(
+        tempInput, delimiter = ',');
+    tempWriter = csv.writer(
+      tempOutput, delimiter = ",", quotechar = '"', quoting = csv.QUOTE_ALL);
+
+    outputTemp = []; #Holds rows for output file
+
+    headings = next(tempReader, None); #header row
+    outputTemp.append(headings);
+    
+    for row in tempReader: #Checking all temp. records
+      for station in stations:
+        if row[1] == station[1]: 
+          row[0] = station[0];
+          break;
+      outputTemp.append(row);
+
+    #Output processed data
+    tempWriter.writerows(outputTemp);
+    tempOutput.close();
+  tempInput.close(); 
+
+print("Classified weather stations")
+
+#Processing insect data
+with open('Insects.csv', 'r') as insectInput:
+  with open('Insects-processed1.csv', 'w') as insectOutput:
+    insectsReader = csv.reader( #Reading raw data
+      insectInput, delimiter = ',');
+    insectsWriter = csv.writer( #To output processed data
+      insectOutput, delimiter = ",", quotechar = '"', quoting = csv.QUOTE_ALL);
+    outputInsects = []; #Stores output 2D array
+    columnsToKeep = [0, 6, 9, 10]; #Features to keep
+
+    headings = next(insectsReader, None); #header row
+    #Adding header row to output.
+    headingsCopy = [];
+    for col in columnsToKeep:
+      headingsCopy.append(headings[col]) 
+    headingsCopy.append('Closest Station');
+    outputInsects.append(headingsCopy);
+
+    for row in insectsReader:
+      shortestDist = (1, 999999999999999999);
+      #Assumes no insect is ^^^^^ degrees away from 
+      #a weather station.
+
+      for station in stations:  
+        #Calculating proximity of current mosquito 
+        #to every weather station
+        DistX = ( float(row[9]) - float(station[1]) ) **2;
+        DistY = ( float(row[10]) - float(station[2]) ) **2;
+        currentDist = ( DistX + DistY ) **0.5;
+
+        if currentDist < shortestDist[1]:
+          #Deciding which weather station is closest 
+          #to current mosquito
+          shortestDist = (station[0], currentDist);
+      
+      #Creating copy of each row with processed data
+      rowcopy = [];
+      for col in columnsToKeep:
+        rowcopy.append(row[col]);
+      rowcopy.append(shortestDist[0]);
+      outputInsects.append(rowcopy);
+    #Outputting processed insect data
+    insectsWriter.writerows(outputInsects);
+    insectOutput.close();
+  insectInput.close();
+  
+print("Calculated Proximity");
+
+#Read Processed Insects file
+with open('Insects-processed1.csv', 'r') as inputIP:
+  IPReader = csv.reader(
+    inputIP, delimiter = ',');
+  #Check if mosquito from new month
+  months = [];
+  numMosquitoesMonthly = {}; #Maps months to insect numbers
+
+  next(IPReader, None);
+  for row in IPReader:
+    currentDate = row[1];
+    if currentDate[:7] not in months:
+      #Add to list of months with data
+      numMosquitoesMonthly[currentDate[:7]] = 1;
+      months.append(currentDate[:7]);
+    else:
+      #Map each month to number of mosquitoes that month
+      for month in numMosquitoesMonthly.keys():
+        if currentDate[:7] == month:
+          count = numMosquitoesMonthly[currentDate[:7]];
+          numMosquitoesMonthly[currentDate[:7]] = count + 1;
+  #Note: Mosquito count categories are 0-100, 101-500,
+  #501-2000, 2001-3500
+  inputIP.close();
+
+#Read Processed PRCP file 
+with open('PRCP-processed1.csv', 'r') as inputPP:
+  PPReader = csv.reader(inputPP);
+  #Preparing final output (csv for Tensorflow)
+  outputSemifinal = [];
+  outputSemifinal.append(["Date", "Latitude", "Longtitude", "Weather Station", "PRCP", "Monthly Mosquito Count"]);
+  
+  next(PPReader, None);
+  for row in PPReader:
+    #If record from one of months with insect data 
+    currentDate = row[4];
+    if currentDate[:7] in numMosquitoesMonthly.keys():
+      #Save date, latitude, longtitude, station ID, 
+      #PRCP, and monthly mosquitoes in 2D array
+      monthlyMosquitoCount = numMosquitoesMonthly[currentDate[:7]];
+      outputRow = [row[4], row[1], row[2], row[0], row[5], monthlyMosquitoCount];
+      outputSemifinal.append(outputRow);
+  
+  inputPP.close();
+
+#Read Temp. file
+with open("Temp-processed1.csv", 'r') as inputTP:
+  TPReader = csv.reader(inputTP);
+  outputFinal = [];
+  outputFinal.append(["Date", "Latitude", "Longtitude", "Weather Station", "PRCP", "Monthly Mosquito Count", "TMAX", "TMIN"]);
+
+  next(TPReader, None);
+  count = 1;
+#If record matches date and ID from PRCP processed data
+  for row in TPReader:
+    if row[4] == outputSemifinal[count][0] and row[0] == outputSemifinal[count][3]:
+      #Append temp data to saved 2D Array
+      rowcopy = outputSemifinal[count];
+      rowcopy.append(row[5]);
+      rowcopy.append(row[6]);
+      outputFinal.append(rowcopy);
+      count += 1;
+  
+  inputTP.close();
+
+for row in range(1, len(outputFinal)):
+  currentRow = outputFinal[row];
+  if currentRow[5] < 101:
+    currentRow[5] = '0-100';
+  elif currentRow[5] > 100 and currentRow[5] < 501:
+    currentRow[5] = '101-500';
+  elif currentRow[5] > 500 and currentRow[5] < 2001:
+    currentRow[5] = '501-2000';
+  elif currentRow[5] > 2000 and currentRow[5] < 3501:
+    currentRow[5] = '2001-3500';
+  
+  #Note: Mosquito count categories are 0-100, 101-500,
+  #501-2000, 2001-3500
+
+#Outputting final processed data for Tensorflow dataset
+with open('Final-processed.csv', 'w') as outputFP:
+  FPWriter = csv.writer(outputFP, delimiter = ",",
+   quotechar = '"', quoting = csv.QUOTE_ALL);
+  
+  FPWriter.writerows(outputFinal);
+
+  #As Mr. Fare said, ALWAYS close your files...
+  outputFP.close();
---
+++ main.py
@@ -0,0 +1,219 @@
+import tensorflow as tf 
+import numpy
+import csv
+
+#Change test, train labels from 0 to 3
+with open('Final-test.csv', 'r') as testInput:
+  #Note: LPO = Labels Processed Output
+  with open('testLPO.csv', 'w') as testOutput:
+    TIReader = csv.reader(testInput); #TI = testInput
+    TOWriter = csv.writer(testOutput); #TO = testOutput
+    header = next(TIReader, None); #Skipping header row
+    #Only keep tmax, tmin, prcp, labels
+    newHeader = [header[5], header[4], header[6], header[7]];
+    labelsPO = []; #PO = Processed Output
+    labelsPO.append(newHeader);
+
+    #Goes through all rows, changing text labels to 
+    #integer values
+    for row in TIReader:
+      if row[5] == '0-100':
+        row[5] = 0;
+      elif row[5] == '101-500':
+        row[5] = 1; 
+      elif row[5] == '501-2000':
+        row[5] = 2;
+      elif row[5] == '2001-3500':
+        row[5] = 3;
+
+      row = [row[5], row[4], row[6], row[7]];
+      labelsPO.append(row);
+
+    TOWriter.writerows(labelsPO); #Output processed file
+    testOutput.close();
+  testInput.close();
+
+with open('Final-train.csv', 'r') as trainInput:
+  with open('trainLPO.csv', 'w') as trainOutput:
+    TrIReader = csv.reader(trainInput); #TrI = trainInput
+    TrOWriter = csv.writer(trainOutput); #TrO = trainOutput
+    header = next(TrIReader, None); #Getting csv header
+    #Only keep tmax, tmin, prcp, labels
+    newHeader = [header[5], header[4], header[6], header[7]];
+    labelsPO = [];
+    labelsPO.append(newHeader); #Add header to new output
+
+    for row in TrIReader:
+      #Note: Label map: 0-100 --> 0
+      if row[5] == '0-100':
+        row[5] = 0;
+      #Note: Label map: 101-500 --> 1
+      elif row[5] == '101-500':
+        row[5] = 1;
+      #Note: Label map: 501-2000 --> 2 
+      elif row[5] == '501-2000':
+        row[5] = 2;
+      #Note: Label map: 2001-3500 --> 3
+      elif row[5] == '2001-3500':
+        row[5] = 3;  
+
+      row = [row[5], row[4], row[6], row[7]];
+      labelsPO.append(row);
+    
+    #Outputting processed file
+    TrOWriter.writerows(labelsPO);
+    trainOutput.close();
+  trainInput.close();
+
+#Convert test, train csv to Tensorflow Dataset
+
+#Getting the csv column headers
+with open('testLPO.csv', 'r') as justGettingHeader:
+  header = justGettingHeader.readline();
+  CSV_COLUMNS = header.rstrip('\n').split(',');
+  justGettingHeader.close();
+
+#Label map described earlier (string to int)
+LABELS = [0, 1, 2, 3];
+LABEL_COLUMN = 'Monthly Mosquito Count'
+FEATURE_COLUMNS = [column for column in CSV_COLUMNS if column != LABEL_COLUMN];
+
+def getDataset(file_path):
+  '''
+  Turns CSV to TF dataset
+  Input: (string) csv filepath 
+  Output: (tuple) list of features, list of labels
+  '''
+  dataset = tf.data.experimental.make_csv_dataset(
+      file_path,
+      batch_size = 100,
+      label_name = LABEL_COLUMN,
+      na_value = "?",
+      num_epochs = 1,
+      ignore_errors = True);
+  return dataset;
+print("a");
+
+rawTrainData = getDataset('trainLPO.csv');
+rawTestData = getDataset('testLPO.csv');
+#Normalise tmax, tmin, prcp data
+
+#Calculating averages for normalisation
+with open('Final-processed.csv', 'r') as findingMeans:
+  meansReader = csv.reader(findingMeans);
+  next(meansReader, None); #Skip csv headers
+
+  #Initialising variables for sums of data
+  sumPRCP = 0; #For precipitation data (5th col)
+  sumTMAX = 0; #For max. temp. data (7th col)
+  sumTMIN = 0; #For min. temp. data (8th col)
+  count = 0; #To track number of values for average
+  
+  #Note: meansReader is iterable object with all
+  #rows of csv file. Each row is a list with each
+  #cell value at an index in that list
+  for row in meansReader: #Goes through all data
+    if len(row[4]) != 0: #Checks record not empty
+      sumPRCP += float(row[4]);
+    if len(row[6]) != 0 and len(row[7]) != 0:
+      sumTMAX += float(row[6]); #Adding to previous total
+      sumTMIN += float(row[7]);  
+    count += 1; #Increasing number of values each time
+  
+  avgPRCP = sumPRCP / count; #Calculating averages
+  avgTMAX = sumTMAX / count;
+  avgTMIN = sumTMIN / count;
+
+  MEANS = { #Holds the mean values for three features
+    'PRCP': avgPRCP, #Used to normalise values later
+    'TMAX': avgTMAX, #Each value represented in relation 
+    'TMIN': avgTMIN  #to extremes (high and low).
+  };
+  findingMeans.close();
+
+def processContinuousData(data, mean):
+  '''
+  Normalises numerical data. 
+  Input: (Tensor) - [-1, 1] data: Type of feature
+  Input: (float) mean: Average of all data values
+  '''
+  # Normalize data
+  data = tf.cast(data, tf.float32) * 1 / (2 * mean);
+  #Tensor shape - no batch size specified, 1 column
+  return tf.reshape(data, [-1, 1]);
+
+#Create 1D tensor of feautures as input
+def preprocess(features, labels):
+  '''
+  Matches normalised data to labels
+  Input: (Tensor - 2D array) Features: Columns with 
+  feature data - PRCP, TMAX, TMIN
+  Input: (Tensor - 1D array) Labels: Column with 
+  mosquito population number - Mosquito Monthly Count
+  '''
+  # Process all continuous features.
+  for feature in MEANS.keys():
+    features[feature] = processContinuousData(features[feature], MEANS[feature]);
+  
+  # Assemble features into a single tensor.
+  features = tf.concat([features[column] for column in FEATURE_COLUMNS], 1);
+  
+  return features, labels;
+
+#Shuffles and processes raw data
+trainData = rawTrainData.map(preprocess).shuffle(500);
+print(trainData);
+testData = rawTestData.map(preprocess);
+
+print("done preprocessing");
+
+def getModel(inputDim, hiddenUnits = [100]):
+  """Create a Keras model with layers.
+
+  Input:
+    inputDim: (int) The shape of an item in a batch. 
+    labels_dim: (int) The shape of a label.
+    hiddenUnits: [int] the layer sizes of the DNN (input layer first)
+    learningRate: (float) the learning rate for the optimizer.
+
+  Output:
+    A Keras model.
+  """
+  #Input layer
+  inputs = tf.keras.Input(shape = (inputDim,));
+  x = inputs;
+  
+  #Hidden Layer
+  for units in hiddenUnits:
+    x = tf.keras.layers.Dense(units, activation='relu')(x);
+  #Output Layer
+  outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x);
+
+  #Entire Model
+  model = tf.keras.Model(inputs, outputs);
+  return model;
+
+print("start training");
+
+#Creating optimiser
+optimiser = tf.keras.optimizers.Adam(
+  learning_rate = 0.0001,
+  name = 'Adam'
+);
+
+model = getModel(3); #3 for input feature data categories
+model.compile(
+    loss = 'binary_crossentropy',
+    optimizer = optimiser,
+    metrics = ['accuracy']);
+
+model.fit(trainData, epochs = 20); #Training model
+print("done training")
+
+#Testing model
+testLoss, testAccuracy = model.evaluate(testData);
+print('\n\nTest Loss {}, Test Accuracy {}'.format(testLoss, testAccuracy)); 
+
+#Creating Predictions using new weights, biases
+predictions = model.predict(testData);
+print(predictions);
